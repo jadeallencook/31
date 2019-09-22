38121b5755098b88d2ace54463bf96f9ea24bcfa
Jade Allen Cook <jadeallencook@gmail.com>
2019-09-22 15:48:00 -0700

    🛣 Setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GameComponent } from './pages/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: MenuComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
