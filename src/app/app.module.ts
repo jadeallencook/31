3d98bfe42f01cd1a5e57b348884e54fa697ca5b9
Jade Allen Cook <jadeallencook@gmail.com>
2019-09-22 15:44:31 -0700

    ⚙️ Generated pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GameComponent } from './pages/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
